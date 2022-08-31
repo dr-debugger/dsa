2cc4bdf4afaad9b91f40100a56696a0899e5bedc
arijit <arijitmaiti@technoexponent.com>
2022-08-31 17:36:05 +0530

    staff changed

--- big0/conclusion.js
+++ big0/conclusion.js
@@ -9,3 +9,36 @@
  * 0(c^n)       exponential
  * 0(n!)        factorial
  */
+
+//❤️ex1
+const f = (m, n) => {
+  for (let i = 0; i < m; i++) {
+    console.log("m");
+  }
+
+  for (let j = 0; j < n; j++) {
+    console.log("n");
+  }
+}; // complexity : 0(m+n)
+
+//❤️ex2
+const crossPairs = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      console.log(arr1[i], arr2[j]);
+    }
+  }
+}; // complexity : 0(m*n)
+
+//❤️ex3
+const foo = (str1, str2) => {
+  if (str1.length > str2.length) {
+    for (let i = 0; i < str1.length; i++) {
+      console.log(str1[i]);
+    }
+  } else {
+    for (let j = 0; j < str2.length; j++) {
+      console.log(str2[j]);
+    }
+  }
+}; // 0(max(m,n)) or 0(n) where n is the length of the longer string
--- big0/exponential.js
+++ big0/exponential.js
@@ -3,27 +3,32 @@
  * 2. ex => 0(2^n), 0(3^n) etc
  */
 
-//❤️ex1 : 0(2^n)
+//❤️ex1 : time complexity : 0(2^n)
 const foo = (n) => {
   if (n === 1) return;
   foo(n - 1);
   foo(n - 1);
 };
+/**
+ * 😎 analyzing stack space for recursion:~
+ *  => the space required on the call stack is the maximum stack depth
+ * **** here space complexity would be 0(n)
+ */
 
-//❤️ex2 : 0(3^n)
+//❤️ex2 :time complexity : 0(3^n) where n > 1
 const bar = (n) => {
   if (n === 1) return;
   bar(n - 1);
   bar(n - 1);
   bar(n - 1);
-};
+}; // here space complexity would be 0(n)
 
-//❤️ex3 : 0(2^n/3) =>  0(2^n)
+//❤️ex3 : time complexity  0(2^n/3) =>  0(2^n)
 const fun = (n) => {
-  if (n === 1) return;
+  if (n <= 0) return;
   fun(n - 3);
   fun(n - 3);
-};
+}; // here space complexity would be 0(n/3 + 1),
 
 /**
  * n  n^2  2^n
