22c435bc7f4bd83f1b194e293b80c95542f1bd8c
arijit <arijitmaiti437@gmail.com>
2022-09-21 13:34:51 +0530

    largest component

--- graphs/challanges/largestCount.js
+++ graphs/challanges/largestCount.js
@@ -1,14 +1,15 @@
 //Write a function, largestComponent, that takes in the adjacency list of an undirected graph. The function should return the size of the largest connected component in the graph.
 
 const obj = {
-  1: ["2"],
-  2: ["1", "8"],
-  6: ["7"],
-  9: ["8"],
-  7: ["6", "8"],
-  8: ["9", "7", "2"],
+  0: ["4", "7"],
+  1: [],
+  2: [],
+  3: ["6"],
+  4: ["0"],
+  6: ["3"],
+  7: ["0"],
+  8: [],
 };
-
 function makeAdjacent(obj) {
   const adjacent = new Map();
 
@@ -23,18 +24,28 @@ const traversal = (graph, visited, node) => {
   if (visited.has(node)) return 0;
 
   visited.add(node);
-  for (let item of graph[node]) {
+  let forThisNode = 1;
+  for (let item of graph.get(node)) {
+    forThisNode += traversal(graph, visited, +item);
   }
+  return forThisNode;
 };
 
 const largestComponent = (graph) => {
   const visited = new Set();
-  let hasNodes = 0;
+  let hasLargeNodes = 0;
 
   for (let [key] of graph) {
     let total = 0;
     total += traversal(graph, visited, key);
+    if (total > hasLargeNodes) {
+      hasLargeNodes = total;
+    }
   }
+
+  return hasLargeNodes;
 };
 
-console.log(largestComponent(makeAdjacent(obj)));
+const graph = makeAdjacent(obj);
+
+console.log(largestComponent(graph));
