eb0bf98f3003ac4c4aa7c89a444b950f46effaa3
arijit <arijitmaiti437@gmail.com>
2023-01-03 10:25:32 +0530

    circlar queue

--- cpp/queue/circularQueue.cpp
+++ cpp/queue/circularQueue.cpp
@@ -6,14 +6,15 @@ using namespace std;
 class CQueue {
   int rear, front, length;
   int *arr;
+  int itemCount;
 
-
-  public:
-    CQueue(int userNum)
+public:
+  CQueue(int userNum)
   {
     rear = front = -1;
     length = userNum;
     arr = new int[userNum];
+    itemCount = 0;
   }
 
   bool isEmpty(){
@@ -33,11 +34,13 @@ class CQueue {
     if(isEmpty()){
       front = rear = 0;
       arr[0] = val;
+      itemCount++;
       return;
     }
 
     rear = (rear + 1) % length;
     arr[rear] = val;
+    itemCount++;
   }
 
   void dequeing(){
@@ -49,10 +52,29 @@ class CQueue {
     if(front == rear){
       cout << "removed the item " << arr[front] << endl;
       front = rear = -1;
+      itemCount--;
       return;
     }
     cout << "removed the item " << arr[front] << endl;
     front = (front + 1) % length;
+    itemCount--;
+  }
+
+  int count (){
+    return itemCount;
+  }
+
+  void display(){
+    if(isEmpty()){
+       cout << "Queue is Empty" << endl;
+      return;
+    }
+
+    for (int i = front; i != rear; (i + 1)% length){
+      cout << arr[i] << endl; // printing front to second last elemnt to rear
+    };
+
+    cout << arr[rear] << endl; // printing last element
   }
 
   ~CQueue(){
