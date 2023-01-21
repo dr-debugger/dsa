4bf86d446ccdeaab6e066d12cc5b75157313d085
arijit <arijitmaiti437@gmail.com>
2023-01-21 19:39:58 +0530

    insertion sort done

--- cpp/Algorithms/sorting/insertion_sort.cpp
+++ cpp/Algorithms/sorting/insertion_sort.cpp
@@ -2,7 +2,52 @@
 #include<string>
 using namespace std;
 
-int main(){
+void insertionSort(int arr[], int size){
+  if(size == 1)
+    return;
+
+    
+  int i, j, val;
+
+  for ( i = 1; i < size; i++)
+  {
+    val = arr[i];
+
+    for (j = i - 1; j >= 0 && arr[j] > val; j--)
+    {
+      arr[j + 1] = arr[j];
+    }
+
+    arr[j + 1] = val;
+  }
+}
+
+void displayArr(int arr[], int s){
+  for (int i = 0; i < s; i++)
+  {
+    cout << arr[i] << endl;
+  }
   
+}
+
+int main(){
+  int arr[5] = {4, 11, 8, 9, 5};
+
+  insertionSort(arr, 5);
+  displayArr(arr, 5);
+
   return 0;
 }
+
+/**
+ * @brief
+ * time complexity O(n^2);
+ *
+ * starting from left -->
+ * pick the second element
+ * compare with all elements on the left. [left side will be sorted]
+ * shift all the element to the sorted sub-list that is greater (or smaller , depending on the order) than the value to be sorted.
+ * insert the value.
+ * do it until all elements are sorted.
+ *
+ */
