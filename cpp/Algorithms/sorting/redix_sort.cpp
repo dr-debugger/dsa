e350e5f20093aa4a8ead37730c0e7b93f832c1b6
arijit <arijitmaiti437@gmail.com>
2023-02-05 17:57:16 +0530

    redix sort

--- cpp/Algorithms/sorting/redix_sort.cpp
+++ cpp/Algorithms/sorting/redix_sort.cpp
@@ -2,14 +2,26 @@
 #include<string>
 using namespace std;
 
+void redixSort(){
+  
+}
+
 int main(){
   
   return 0;
 }
 
-
 /**
- * @brief 
+ * @brief
+ *
+ * Time complexity : O(n+k)
+ * Space complexity : O(n+k)
+ *
+ *
+ * its an non-comparative sorting algorithm.
+ * It vaoids comparison by creating and distributing elements into buckets accorsing to their redix
+ * Typically redix sort uses counting sort as a subroutine to sort.
  * 
- * 
+ *
+ *
  */
